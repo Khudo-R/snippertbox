89e6bac8bd2912fa1ea4f8d072b717920e99c87e
Khudo #R <mirkhud97@gmail.com>
2025-09-15 18:20:46 +0900

    Handle runtime error

--- cmd/web/helper.go
+++ cmd/web/helper.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
-	// "runtime/debug"
 )
 
 func (app *application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
@@ -14,11 +14,17 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 		return
 	}
 
-	w.WriteHeader(status)
-	err := ts.ExecuteTemplate(w, "base", data)
+	buf := new(bytes.Buffer)
+
+	err := ts.ExecuteTemplate(buf, "base", data)
 	if err != nil {
 		app.serverError(w, r, err)
+		return
 	}
+
+	w.WriteHeader(status)
+
+	buf.WriteTo(w)
 }
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
